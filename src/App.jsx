67845da5e1739f06be9e19e67a400d2f4c25fb2a
Ömer Faruk TARTAR <oftartar@gmail.com>
2024-07-30 12:44:36 +0300

    1- initialized project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
