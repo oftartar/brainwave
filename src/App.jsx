1ecc9b2cd75fdbbcb1e18716572fb35745d1324b
Ömer Faruk TARTAR <oftartar@gmail.com>
2024-10-03 14:05:15 +0300

    implement header menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
 
       <ButtonGradient />
     </>
